caa3204f6845d05a67aee3d2e90fdfed2dc8adbc
Habner Phillippe <110646680+HabnerPhillippe@users.noreply.github.com>
2022-10-04 01:22:21 -0300

    adicionando Delete e Put

--- src/main/java/br/residencia/api/aula02/controller/AlunoController.java
+++ src/main/java/br/residencia/api/aula02/controller/AlunoController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -32,15 +34,23 @@ public class AlunoController {
         return alunos;
     }
 
-
+    //localhost:8080/alunos/123 (@pathvariable)
     @GetMapping("{matricula}")
     public Aluno buscar(@PathVariable String matricula) {
+
+        //método 1
         for (int i = 0; i < alunos.size(); i++) {
 			if (alunos.get(i).getMatricula().equals(matricula)) {
 				return alunos.get(i);
 			}
 		}
 		return null;
+
+        //método 2 -> programação funcional
+        // return alunos.stream()
+        // .filter(a->a.getMatricula().equals(matricula))  //a: variável qualquer
+        // .findFirst()
+        //  .orElse(null);
     }
 
 
@@ -50,4 +60,29 @@ public class AlunoController {
         alunos.add(aluno);
         return aluno;
     }
+
+    @DeleteMapping("{matricula}")
+    public void apagar(@PathVariable String matricula){
+
+        for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).getMatricula().equals(matricula)) {
+				alunos.remove(i);
+			}
+		}
+    }
+
+
+    @PutMapping("{matricula}")
+    public Aluno atualizar(@RequestBody Aluno aluno, @PathVariable String matricula){
+
+        for (int i = 0; i < alunos.size(); i++) {
+			if (alunos.get(i).getMatricula().equals(matricula)) {
+				alunos.set(i,aluno);
+                return aluno;
+			}
+		}
+        return null;
+    }
+    
+
 }
